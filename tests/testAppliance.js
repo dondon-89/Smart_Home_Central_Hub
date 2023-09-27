1181080b3fbb09e599d26644409fe0cb61e497dd
bwilkins96 <ben.jr.speed@gmail.com>
2023-09-27 12:09:19 -0400

    added before hook

--- tests/testAppliance.js
+++ tests/testAppliance.js
@@ -4,30 +4,32 @@ const assert = require('assert');
 const { SmartAppliance } = require('../classes/appliance');
 
 describe('SmartAppliance', () => {
-    const testAppliance = new SmartAppliance();
+    before(() => {
+        this.appliance = new SmartAppliance();
+    })
 
     it('should connect when connect method is called', () => {
-        testAppliance.connect();
-        assert.equal(testAppliance.connected, true);
+        this.appliance.connect();
+        assert.equal(this.appliance.connected, true);
     });
 
     it('should disconnect when disconnect method is called', () => {
-        testAppliance.disconnect();
-        assert.equal(testAppliance.connected, false)
+        this.appliance.disconnect();
+        assert.equal(this.appliance.connected, false)
     });
 
     it('should power on when powerOn method is called', () => {
-        testAppliance.powerOn();
-        assert.equal(testAppliance.poweredOn, true);
+        this.appliance.powerOn();
+        assert.equal(this.appliance.poweredOn, true);
     });
 
     it('should power off when powerOff method is called', () => {
-        testAppliance.powerOff();
-        assert.equal(testAppliance.poweredOn, false);
+        this.appliance.powerOff();
+        assert.equal(this.appliance.poweredOn, false);
     });
 
     it('should be able to set a custom name', () => {
-        testAppliance.name = 'Bingo';
-        assert.equal(testAppliance.name, 'Bingo');
+        this.appliance.name = 'Bingo';
+        assert.equal(this.appliance.name, 'Bingo');
     })
 });
